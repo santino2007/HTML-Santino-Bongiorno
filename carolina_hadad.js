81b7e806773bfde0238c2abac76c835507eb5e55
santino2007 <santinobongiorno2007@gmail.com>
2024-10-17 12:45:09 -0300

    Add files via upload

--- carolina_hadad.js
+++
@@ -1,9 +0,0 @@
-function muestra_oculta(id){
-    let div = document.getElementById(id)
-    if (div.style.display == 'none'){
-        div.style.display='flex'
-    }
-    else{
-        div.style.display="none"
-    }
-}
